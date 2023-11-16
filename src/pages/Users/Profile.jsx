aa9a3e2f8047071cb80adab4198845864b43f005
Sachin Sagar <sachinssagar@gmail.com>
2023-11-16 17:32:42 +0530

    Profile

--- src/pages/Users/Profile.jsx
+++ src/pages/Users/Profile.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../../components/Spinner.jsx";
 import { VITE_API } from "../../App.jsx";
@@ -7,12 +7,13 @@ import { VITE_API } from "../../App.jsx";
 export const Profile = () => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [login, setLogin] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
-      alert("Please login first");
-      return;
+      navigate("/login");
     }
 
     axios
@@ -23,11 +24,12 @@ export const Profile = () => {
       })
       .then((res) => {
         setIsLoading(false);
-        setUser(res.data);
+        setUser(res.data.user);
+        setLogin(true);
       })
       .catch((err) => {
         setIsLoading(false);
-        alert("Something went wrong");
+        setLogin(false);
         console.log(err);
       });
   }, []);
@@ -37,22 +39,29 @@ export const Profile = () => {
       <h2 className="text-2xl font-bold mb-4">User Profile</h2>
       {isLoading ? (
         <Spinner />
+      ) : login ? (
+        <div>
+          <p className="mb-2">
+            <span className="font-bold">Name:</span> {user.name}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">Email:</span> {user.email}
+          </p>
+          <Link to="/update">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Update Profile
+            </button>
+          </Link>
+        </div>
       ) : (
-        user && (
-          <div>
-            <p className="mb-2">
-              <span className="font-bold">Name:</span> {user.name}
-            </p>
-            <p className="mb-2">
-              <span className="font-bold">Email:</span> {user.email}
-            </p>
-            <Link to="/update">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Update Profile
-              </button>
-            </Link>
-          </div>
-        )
+        <div>
+          <p>You are not logged in</p>
+          <Link to="/login">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Login
+            </button>
+          </Link>
+        </div>
       )}
     </div>
   );
