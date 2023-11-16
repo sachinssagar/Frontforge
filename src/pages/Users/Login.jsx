d718b0267335f53bf23b06fbc03e6c71bf6227fe
Sachin Sagar <sachinssagar@gmail.com>
2023-11-16 16:34:53 +0530

    Profile

--- src/pages/Users/Login.jsx
+++ src/pages/Users/Login.jsx
@@ -1,49 +1,93 @@
+import { useState } from "react";
+import axios from "axios";
+import Spinner from "../../components/Spinner.jsx";
+import { Link, useNavigate } from "react-router-dom";
+import { VITE_API } from "../../App.jsx";
+
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+    setIsLoading(true);
+    axios
+      .post(`${VITE_API}/auth/login`, data)
+      .then((res) => {
+        setIsLoading(false);
+        localStorage.setItem("token", res.data.token);
+        navigate("/profile");
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        alert("Something went wrong");
+        console.log(err);
+      });
+  };
+
   return (
-    <>
+    <div className="max-w-md mx-auto mt-8 p-4 border rounded bg-white">
       <h2 className="text-2xl font-bold mb-4">Login</h2>
-      <form className="max-w-md mx-auto">
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Email address
-          </label>
-          <input
-            className="border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="mb-4">
-          <input className="mr-2 leading-tight" type="checkbox" id="checkbox" />
-          <label className="text-sm" htmlFor="checkbox">
-            Remember me
-          </label>
-        </div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-        >
-          Login
-        </button>
-      </form>
-    </>
+      {isLoading && <Spinner />}
+      {!isLoading && (
+        <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Login
+            </button>
+            <Link
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800  cursor-pointer"
+              to="/register"
+            >
+              Register
+            </Link>
+          </div>
+        </form>
+      )}
+    </div>
   );
 };
--- src/pages/Users/Profile.jsx
+++ src/pages/Users/Profile.jsx
@@ -1,32 +1,59 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../../components/Spinner.jsx";
+import { VITE_API } from "../../App.jsx";
 
 export const Profile = () => {
-  // Replace this with actual user data from your authentication context
-  const user = {
-    displayName: "John Doe",
-    email: "john.doe@example.com",
-  };
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      alert("Please login first");
+      return;
+    }
+
+    axios
+      .get(`${VITE_API}/auth/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setIsLoading(false);
+        setUser(res.data);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        alert("Something went wrong");
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="max-w-md mx-auto mt-8 p-4 border rounded bg-white">
-      <h2 className="text-2xl font-bold mb-4">Profile</h2>
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Display Name:
-        </label>
-        <p>{user.displayName}</p>
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Email Address:
-        </label>
-        <p>{user.email}</p>
-      </div>
-      <Link to="/update">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Update Profile
-        </button>
-      </Link>
+      <h2 className="text-2xl font-bold mb-4">User Profile</h2>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        user && (
+          <div>
+            <p className="mb-2">
+              <span className="font-bold">Name:</span> {user.name}
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">Email:</span> {user.email}
+            </p>
+            <Link to="/update">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Update Profile
+              </button>
+            </Link>
+          </div>
+        )
+      )}
     </div>
   );
 };
