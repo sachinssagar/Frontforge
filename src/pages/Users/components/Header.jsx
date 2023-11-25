824ae0ff174a2a1ea8cb3a4d4ffde98d34bc3c0c
Sachin Sagar <sachinssagar@gmail.com>
2023-11-25 11:52:07 +0530

    navigation

--- src/pages/Users/components/Header.jsx
+++ src/pages/Users/components/Header.jsx
@@ -1,12 +1,38 @@
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+import { VITE_API } from "../../../App.jsx";
 
 export const Header = () => {
+  const [user, setuser] = useState(null);
+  const isLoggedIn = user !== null;
   const navigate = useNavigate();
   const logout = () => {
-    localStorage.clear();
+    localStorage.clear("token");
+    setuser(null);
     navigate("/login");
   };
-  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        const response = await fetch(`${VITE_API}/auth/profile`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.ok) {
+          const userData = await response.json();
+          setuser(userData.user);
+        } else {
+          navigate("/login");
+        }
+      }
+    };
+    checkAuth();
+  }, [navigate]);
 
   return (
     <header className="bg-gray-800 text-white p-4">
@@ -16,9 +42,9 @@ export const Header = () => {
         </Link>
         <nav>
           <ul className="flex space-x-4">
-            {token ? (
+            {isLoggedIn ? (
               <>
-                <li>Hi {JSON.stringify(token).name} </li>
+                <li>Hi {user.name} </li>
 
                 <li>
                   <Link to="/profile">Profile</Link>
